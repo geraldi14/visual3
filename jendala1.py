08e564c3addfef02a51797cd6594bc4d86ecc8a1
geraldi14 <150001999+geraldi14@users.noreply.github.com>
2024-10-18 20:33:19 +0800

    Add files via upload

---
+++ jendala1.py
@@ -0,0 +1,4 @@
+import PysimpleGUI as sg
+window = sg.Window(tittle ="latihan pertama",layout = [[]],size=(400,200))
+window()
+window.close()
---
+++ jendala2.py
@@ -0,0 +1,4 @@
+import PySimpleGUI as sg  
+window = sg.Window(title ="latihan pertama", layout = [[sg.Text("Selamat belajar PySimpleGUI Geraldi")]],size=(400,200))
+window()
+Window.close()
